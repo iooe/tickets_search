60972eaa58e1495c4b1ea693949a5f7f21fac884
Roman Yepanchenko <podstavat@gmail.com>
2023-10-30 12:29:39 +0100

    feat: add price limit for search engine as argument

--- Core/Core.clj
+++ Core/Core.clj
@@ -2,70 +2,76 @@
   (:require [clojure.string :as string :only [upper-case]])
   )
 
+(def maxPrice 10001000)
 (defn process
   [allNodes, currentNode, fromNode, toNode, path, price, deadEnds]
 
- (let [
-       isFoundResult (= currentNode toNode)
-       isCompleted  (= (count path) 0)
-       theLowestPrice (atom -1)
-       theLowestPricedNode (atom nil)
-       currentNodeConnections (get allNodes currentNode)
-       ]
+  (let [
+        isFoundResult (= currentNode toNode)
+        isCompleted (= (count path) 0)
+        theLowestPrice (atom -1)
+        theLowestPricedNode (atom nil)
+        currentNodeConnections (get allNodes currentNode)
+        ]
 
-   (if (or (= isFoundResult true) (= isCompleted true))
-     (do
-       {
-        "path" path,
-        "price" price
-        }
-       )
-     (do
-       (doseq [node currentNodeConnections]
-         (if
-           (and
-             ; if path not contains (get node "node")
-             (not (= (some #(= (get node "connection") %) path) true))
-             ; if deadEnds not contains (get node "node")
-             (not (= (some #(= (get node "connection") %) deadEnds) true))
-             ; if theLowestPricedNode is nil, or we found a new lowest price
-             (or
-               (= @theLowestPricedNode nil)
-               (< (get node "price") @theLowestPrice)
-               )
-             )
-           ; if we complete statements, then update theLowestPricedNode and theLowestPrice
-           (do
-             (reset! theLowestPricedNode (get node "connection"))
-             (reset! theLowestPrice (get node "price"))
-             )
-           )
-         )
+    (if (or (= isFoundResult true) (= isCompleted true))
+      (do
+        {
+         "path"  path,
+         "price" price
+         }
+        )
+      (do
+        (doseq [node currentNodeConnections]
+          (if
+            (and
+              ; if path not contains (get node "node")
+              (not (= (some #(= (get node "connection") %) path) true))
+              ; if deadEnds not contains (get node "node")
+              (not (= (some #(= (get node "connection") %) deadEnds) true))
+              ; if theLowestPricedNode is nil, or we found a new lowest price
+              (or
+                (= @theLowestPricedNode nil)
+                (< (get node "price") @theLowestPrice)
+                )
+              )
+            ; if we complete statements, then update theLowestPricedNode and theLowestPrice
+            (do
+              (reset! theLowestPricedNode (get node "connection"))
+              (reset! theLowestPrice (get node "price"))
+              )
+            )
+          )
 
-       ; if the lowest price is -1 that means that there no any suitable connection for the path building
-       (if (= @theLowestPrice -1)
-         ; then
-         (process allNodes,
-                      (get path (- (count path) 2)),               ; use the parent node as the next main node
-                      fromNode,
-                      toNode,
-                      (into [] (drop-last 1 path))                 ; remove the last node from current path
-                      (into [] (drop-last 1 price))
-                      (conj deadEnds currentNode)                  ; add current node to the deadEnds
-                      )
-         ; else
-         (process allNodes,
-                      @theLowestPricedNode,                         ; use the lowest priced connection of the current node as the next main node
-                      fromNode,
-                      toNode,
-                      (conj path @theLowestPricedNode)              ; add the lowest priced connection of the current node to the path
-                      (conj price @theLowestPrice)
-                      deadEnds
-                      )
-         )
-       )
-     )
-   )
+        (println "in process" price)
+
+        (if (> (+ (apply + price) @theLowestPrice) maxPrice)
+          (println "BACK BECAUSE OUT OF THE MAX PRICE")
+          )
+        ; if the lowest price is -1 that means that there no any suitable connection for the path building
+        (if (or (= @theLowestPrice -1) (> (+ (apply + price) @theLowestPrice) maxPrice))
+          ; then
+          (process allNodes,
+                   (get path (- (count path) 2)),           ; use the parent node as the next main node
+                   fromNode,
+                   toNode,
+                   (into [] (drop-last 1 path))             ; remove the last node from current path
+                   (into [] (drop-last 1 price))
+                   (conj deadEnds currentNode)              ; add current node to the deadEnds
+                   )
+          ; else
+          (process allNodes,
+                   @theLowestPricedNode,                    ; use the lowest priced connection of the current node as the next main node
+                   fromNode,
+                   toNode,
+                   (conj path @theLowestPricedNode)         ; add the lowest priced connection of the current node to the path
+                   (conj price @theLowestPrice)
+                   deadEnds
+                   )
+          )
+        )
+      )
+    )
   )
 
 
@@ -74,8 +80,9 @@
   ; Connections represents as a price and a node attributes.
   ;
   ; {"string": [{"price": int, "node": string}]}, string, string
-  [nodes, fromNode, toNode]
+  [nodes, fromNode, toNode, maxPrice]
 
+  (def maxPrice maxPrice)
   ; start recursive function
   (process nodes (string/upper-case fromNode) (string/upper-case fromNode) (string/upper-case toNode) [(string/upper-case fromNode)] [] [])
   )
