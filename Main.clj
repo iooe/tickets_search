c1eab8e7cd6a545903e9c8e013c75dd264ed7597
Roman Yepanchenko <podstavat@gmail.com>
2023-11-08 12:45:44 +0100

    Improvement: add response filtering by price and length

--- Main.clj
+++ Main.clj
@@ -4,9 +4,38 @@
   )
 
 
-(run! println (SearchEngineCore3/init
-           (CsvDataAdapter3/adapter "Datasets/flights-ICA1.csv")
-           "PRAGUE"
-           "ZAGREB"
-           )
-         )
+(println "List of all routes")
+
+(def LIMITS_PRICE {"f" 700 "g" 1000})
+(def LIMITS_LENGTH {"f" 3 "g" 4})
+(let [
+      type "g"
+      priceLimit (atom 0)
+      lengthLimit (atom 0)
+      routes (atom [])
+      ]
+
+  (reset! priceLimit (get LIMITS_PRICE type))
+  (reset! lengthLimit (get LIMITS_LENGTH type))
+
+  (println "====================")
+  (println "ALL ROUTES")
+  (reset! routes (SearchEngineCore3/init
+                  (CsvDataAdapter3/adapter "Datasets/flights-ICA1.csv")
+                  "Prague"
+                  "Berlin"
+                  )
+        )
+  (run! println @routes)
+
+  (println "====================")
+  (println "FILTERED BY PRICE")
+  (reset! routes (filter  #(<= (:priceSum %) @priceLimit) @routes))
+  (run! println (filter  #(<= (:priceSum %) @priceLimit) @routes))
+
+  (println "====================")
+  (println "FILTERED BY PRICE AND LENGTH")
+
+  (reset! routes (filter  #(<= (:length %) @lengthLimit) @routes))
+  (run! println (filter  #(<= (:length %) @lengthLimit) @routes))
+  )
