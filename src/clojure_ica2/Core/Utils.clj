e61ee5da064915b7163aced6eafb4dcd8f7391ab
Roman Yepanchenko <podstavat@gmail.com>
2023-12-26 18:25:01 +0100

    feature: Core/Utils; averageByDepartureDestinationPrice

---
+++ src/clojure_ica2/Core/Utils.clj
@@ -0,0 +1,26 @@
+(ns clojure_ica2.Core.Utils)
+
+(defn averageByDepartureDestinationPrice [values departure destination]
+
+  (let [
+        depdes (atom {})
+        temp (atom {})
+        ]
+
+    (doseq [value @values]
+      (reset! temp (str (:departure value) (:destination value)))
+      (if (contains? @depdes @temp)
+        (do
+          (reset! depdes (assoc @depdes @temp (conj (get @depdes @temp) (:paid value))))
+          )
+        (do
+          (reset! depdes (assoc @depdes @temp [(:paid value)]))
+          )
+        )
+      )
+
+    ;;depdes
+
+    (float (/ (apply + (get @depdes (str departure destination))) (count (get @depdes (str departure destination)))))
+    )
+  )
