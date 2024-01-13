2c1b75975c0d79bc81456fab6de3c7d6a31755ed
Roman Yepanchenko <podstavat@gmail.com>
2024-01-13 09:09:46 +0100

    feature: add initial entry point of the app "Maiin/prepare_travel_plan"

--- src/clojure_ica2/Main.clj
+++ src/clojure_ica2/Main.clj
@@ -1,29 +1,39 @@
 (ns clojure_ica2.Main
   (:require
-    [clojure_ica2.Adapters.CsvDataAdapter :as CsvDataAdapter]
-    [clojure.string :as string :only [split]]
-            )
+    [clojure.string :only [split]]
+    [clojure_ica2.Store :as Store]
+   )
   )
 
+(defn prepare_travel_plan
+  [departure destination people]
 
-(def isDebug true)
-(def filePath "")
-(defn prepare-travel-plan
-  []
-  (let [
+  (if (= (count people) 0)
+    (do
+      (let [
+            avgPrices (atom [])
+            family (get (clojure.string/split (get (get people 0) 0) #" ") 0)
+            isFamily (atom true)
+            response (atom 9999999999)
+            ]
 
-        ]
+        (doseq [value people]
+          (reset! avgPrices (conj @avgPrices (Store/getMedianByYob (get value 1))))
+          (reset! avgPrices (conj @avgPrices (Store/getMedianByDepDes departure destination)))
 
-    (run! println (CsvDataAdapter/adapter (get (string/split (str *ns*) #"\.") 0) filePath))
+          (if (not (= family (get (clojure.string/split (get value 0) #" ") 0)))
+            (reset! isFamily false)
+            )
+          )
 
+        (reset! response (float (/ (apply + @avgPrices) (count @avgPrices))))
+
+        (if (= @isFamily true)
+          (float (/ @response 1))
+          (float (/ @response 1.4))
+          )
+        )
+        (println "[people] argument is empty")
+      )
     )
   )
-
-;==========
-;
-;===INIT===
-;
-;==========
-(def filePath "Datasets/sales_team_1.csv")
-
-(prepare-travel-plan)
